cfca22be13cea76672f2e0aaf3444e353f59992d
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2018-11-19 12:07:25 -0500

    Add files via upload

---
+++ Self-divisor/SelfDivisor.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class SelfDivisor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SelfDivisor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SelfDivisor
+     */
+    public static boolean isSelfDivisor(int number)
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Self-divisor/runner.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class runner
+{
+    public static void main(String[] args)
+    {}
+}
